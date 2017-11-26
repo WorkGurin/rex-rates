207bb6d7936bc81d87a3aa50149eec3bcb9b6886
Stanislav Gurin <work.gurin@gmail.com>
2017-11-26 14:08:41 +0200

    initial commit

---
+++ src/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Graph from './components/Graph';
+
+String.prototype.capitalize = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+};
+
+String.prototype.decapitalize = function () {
+    return this.charAt(0).toLowerCase() + this.slice(1);
+};
+
+ReactDOM.render(
+    <Graph url="https://raw.githubusercontent.com/janaipakos/react-d3-data-viz/master/public/data/data.csv" />,
+    document.getElementById('graph'));
---
+++ webpack.production.config.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var path = require('path');
+var webpack = require('webpack');
+var config = require('./config.json');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+let extractCSS = new ExtractTextPlugin('[name].css');
+
+module.exports = {
+    devtool: 'source-map',
+    entry: ['./src/index', './css/style.less'],
+    output: {
+        path: path.join(__dirname, config.publicFolder),
+        publicPath: '/' + config.publicFolder + '/',
+        filename: 'bundle.js'
+    },
+    plugins: [
+        extractCSS,
+        new webpack.optimize.DedupePlugin()
+    ],
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    module: {
+        loaders: [{
+            test: /\.js|\.jsx$/,
+            loaders: ['babel'],
+            exclude: /node_modules/
+        },
+        {
+            test: /\.less$/,
+            exclude: /node_modules/,
+            loader: ExtractTextPlugin.extract('style-loader', 'css-loader!less-loader')
+        }
+        ]
+    }
+};
